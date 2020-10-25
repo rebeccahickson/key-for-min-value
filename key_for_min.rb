bca1f62069be8ae12adf4a134e188f7fa3469b40
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-10-25 18:34:51 -0400

    commit tree 23eb262794d0dea9da0ed65c5b617df4565c6f1f

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,12 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
+    return nil if name_hash.length == 0
+    lowest_name = name_hash.first[0]
+    lowest_value = name_hash[lowest_name]
+    name_hash.collect do |name, age|
+        if age < lowest_value
+            lowest_name = name
+            lowest_value = age
+        end
+    end
+lowest_name
 end
